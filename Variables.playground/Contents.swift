5ae7413f6f2d9a74efd91f2811c58d818b6c3ec2
Tyler Lutz <tlutz.computers@gmail.com>
2016-02-10 22:45:15 -0500

    Completed Variabels Playground

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var welcomeMessage = "Hello, playground"
+
+var number = 20
+
+var decimal = 50.5
+
+decimal = 20.23
+
+let newDecimal = 40.5
+
+var 🙈 = "Horse"
+
+var account1 = 0.0, account2 = 1.1, account3 = 3.6
+
+//Type Annotation
+var newMessage: String
+
+newMessage = "New Message"
+
+var red, green, blue: Double
+
+red = 20.25
