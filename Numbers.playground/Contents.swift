d78f1343517747a8d09ffd0959d0b517f30d096f
Tyler Lutz <tlutz.computers@gmail.com>
2016-02-10 23:07:57 -0500

    Finished Numbers Playground

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Integers
+var myBankAccount: Int = -500
+
+//Unsigned Int value has to be zero or greater
+var myAge: UInt = 22
+
+//Use Int64 for numebrs larger then 2,147,483,647
+var bigNumber: Int64 = 2147483647
+
+//Double up to 15 decimal points
+var anotherBankAccount: Double = 55.5
+
+//Float up to 6 decimal points
+var someVal: Float = 5.5
+
+//Have to cast someVal to a double
+var sum = anotherBankAccount * Double(someVal)
