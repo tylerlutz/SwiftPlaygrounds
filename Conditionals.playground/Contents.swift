1cb63ed57ba09f8ece2e890d1fd7426d8067e414
Tyler Lutz <tlutz.computers@gmail.com>
2016-02-11 20:52:50 -0500

    Adding new playgrounds

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var isMyHouseOnFire: Bool = false
+
+
---
+++ MathExercise2.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var experience = 15
+
+var retirement = 5
+
+var untilRetirement = experience + retirement
+
+var multi = 9 * 5
+
+var sub = experience - retirement
+
+var div = experience / retirement
+
